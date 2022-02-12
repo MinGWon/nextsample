1bf0808781d9e7a14ace016af48a52f491036b31
MinGWon <40914543+MinGWon@users.noreply.github.com>
2022-02-12 12:50:28 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Hello</code>
+          <code>Hello</code>
         </p>
       </main>
 
